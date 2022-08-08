8277bd1c7316877ff987d5351abc041f885f56b0
Diana <dicomeinit@gmail.com>
2022-08-08 17:53:11 +0300

    Score Keeping and Changing the Ball Speed

--- ball.py
+++ ball.py
@@ -8,8 +8,9 @@ class Ball(Turtle):
         self.color("white")
         self.shape("circle")
         self.penup()
-        self.x_move = 10
-        self.y_move = 10
+        self.x_move = 3
+        self.y_move = 3
+        self.move_speed = 0.1
 
     def move(self):
         new_x = self.xcor() + self.x_move
@@ -21,7 +22,9 @@ class Ball(Turtle):
 
     def bounce_x(self):
         self.x_move *= -1
+        self.move_speed *= 0.9
 
     def reset_position(self):
         self.goto(0, 0)
+        self.move_speed = 0.1
         self.bounce_x()
